b45f4b6860ddd66a9d2e4067ae6818c0bbf316f8
sauvik <sauvik@logic-square.com>
2026-03-31 20:14:14 +0530

    feat: Add new book quizzes for "Chamber of Secrets" and "Philosopher's Stone"
    - Introduced two new quizzes: Chamber of Secrets Quiz and Philosopher's Stone Quiz, enhancing the quiz collection.
    - Updated the quizzes index to include the new quizzes while commenting out the short quiz for now.

--- src/data/quizzes/index.ts
+++ src/data/quizzes/index.ts
@@ -1,5 +1,13 @@
 import { HpGuessMoment1 } from "./hp-trivia";
-import { hpShortQuiz } from "./hp-short-quiz";
+// import { hpShortQuiz } from "./hp-short-quiz";
 import { hpMediumHard1 } from "./hp-medium-hard-1";
+import { philosophersStoneBookQuiz1 } from "./book quizzes/philosphers-stone-1";
+import { chamberOfSecretsBookQuiz1 } from "./book quizzes/chamber-of-secrets-1";
 
-export const quizzes = [HpGuessMoment1, hpMediumHard1, hpShortQuiz];
+export const quizzes = [
+  HpGuessMoment1,
+  hpMediumHard1,
+  //   hpShortQuiz,
+  philosophersStoneBookQuiz1,
+  chamberOfSecretsBookQuiz1,
+];
